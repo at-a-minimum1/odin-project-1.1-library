d71bc1e3bf6682620dbfe9e247d38b41751fc9ec
Timothy Beler <timothybeler4@gmail.com>
2022-11-19 03:50:55 -0500

    Added more styling and a form to the right. Possibly going to move the nav bar to the left for easier styling

--- app.js
+++ app.js
@@ -25,3 +25,29 @@ function addElement() {
 	resultsPanel.appendChild(newCard);
 	console.log("Button-works");
 }
+
+function changeTheme(primary, secondary, accent) {
+	primary = this.primary;
+	secondary = this.secondary;
+	accent = this.accent;
+	if (primary == null) {
+		primary = "#D4F2FC";
+		secondary = "#B5A081";
+		accent = "#FF6240";
+	}
+
+	let root = document.documentElement;
+	root.addEventListener("click", (e) => {
+		root.style.setProperty("--primary-clr", primary);
+		root.style.setProperty("--secondary-clr", secondary);
+		root.style.setProperty("--accent-clr", accent);
+	});
+}
+
+// Example code for how to update CSS variables. Use this for updating the themes
+// let root = document.documentElement;
+
+// root.addEventListener("mousemove", e => {
+//   root.style.setProperty('--mouse-x', e.clientX + "px");
+//   root.style.setProperty('--mouse-y', e.clientY + "px");
+// });
